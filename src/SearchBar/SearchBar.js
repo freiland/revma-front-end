8b8a423b1b8a1ada7fe7ce7d95719eae4667b659
Forrest Reiland <you@example.com>
2020-12-18 15:34:46 -0800

    adjust default map zoom

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -11,23 +11,25 @@ import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import TextField from '@material-ui/core/TextField';
 
 
-export default function SearchBar() {
+const SearchBar = ({keyword,setKeyword}) => {
+  const BarStyling = {width:"70rem",background:"#F2F1F9", border:"none", padding:".4rem"};
+  
   return (
+    
+    <input 
+    style={BarStyling}
+    key="random1"
+    value={keyword}
+    placeholder={"search a city"}
+    onChange={(e) => setKeyword(e.target.value)}
+   />
+  
+  );
+}
 
-<div>
-    <div>
-      <SearchIcon /> 
-    </div>
-    <form noValidate autoComplete="off">
-  <TextField id="standard-basic" label="search for a city" margin='normal' />
-  </form>
-  </div> 
-
-  )
-          }
+export default SearchBar
