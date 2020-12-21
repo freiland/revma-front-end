7dd125496a1f6e06402c3bdb0ebf882946d1fedd
Forrest Reiland <you@example.com>
2020-12-21 11:22:42 -0700

    add new image, work on resizing image

--- src/Splash/Splash.js
+++ src/Splash/Splash.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import image from '../img/splash_image.jpg';
+import image from '../img/splash_four.jpg';
 
 export default function Splash () {
   return (
+    <div className = 'image' >
     <img id='skyline' src={image} />
+    <h1>test</h1>
+    </div>
 
   )
 
