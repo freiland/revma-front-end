8877cdf555b4c75e833e39aa0ae73e0142b540a3
Forrest Reiland <you@example.com>
2020-12-21 11:54:35 -0700

    Add css styling to splash page

--- src/Splash/Splash.js
+++ src/Splash/Splash.js
@@ -4,8 +4,12 @@ import image from '../img/splash_five.jpg';
 export default function Splash () {
   return (
     <div className = 'image' >
+      <h1> test</h1>
+      {/* <div className ='text'>
+      
+      </div> */}
     <img id='skyline' src={image} />
-    <h1>test</h1>
+    
     </div>
 
   )
