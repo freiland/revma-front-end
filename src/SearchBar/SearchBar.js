100b27f675ed0e56aa3811e5ac1764e5e554aaf5
Forrest Reiland <you@example.com>
2020-12-22 12:55:39 -0700

    remove old search bar, search material ui for new one

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -21,14 +21,7 @@ const SearchBar = ({keyword,setKeyword}) => {
   
   return (
     
-    <input 
-    style={BarStyling}
-    key="random1"
-    value={keyword}
-    placeholder={"search a city"}
-    onChange={(e) => setKeyword(e.target.value)}
-   />
-  
+    
   );
 }
 
