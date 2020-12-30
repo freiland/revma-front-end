7c78d4aaa14d6e1cab7a6e244d51c16cd69f6a63
Forrest Reiland <you@example.com>
2020-12-30 14:41:22 -0800

    add second search bar with text input field

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -3,19 +3,17 @@ import React from 'react';
 
 function Search() {
   
-  doSomethingWith() {
-    console.log(hi); 
-   } 
-    
+  
 
     
   
 return (
-  <SearchBar
-    value={this.state.value}
-    onChange={(newValue) => this.setState({ value: newValue })}
-    onRequestSearch={() => doSomethingWith(this.state.value)}
-  />
+  <input type='text' />
+  // <SearchBar
+    
+  //   onChange={(newValue) => this.setState({ value: newValue })}
+  //   //onRequestSearch={() => doSomethingWith(this.state.value)}
+  // />
 );
 };
 
