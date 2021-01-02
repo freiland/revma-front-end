1c112c6956659cf86699d21fc7ecb4e3b24642bf
Forrest Reiland <you@example.com>
2021-01-02 14:07:38 -0800

    add fetch api call to useeffect hook

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -13,6 +13,19 @@ console.log(searchTerm)
 const cityInput = setSearchTerm 
 
 useEffect(() => {
+  fetch(`https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${searchTerm}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "7043bd60e6msh017de187a06b311p16fbfejsn6f5cf5064028",
+		"x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
   
 }
 
