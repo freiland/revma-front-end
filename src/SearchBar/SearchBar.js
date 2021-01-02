311fa4209af1146eaa80caf785a5417a325e02ad
Forrest Reiland <you@example.com>
2021-01-02 13:24:42 -0800

    add usestate and useeffect hook to search component

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,18 +1,19 @@
 import SearchBar from 'material-ui-search-bar';
 import React from 'react';
+import { useState, useEffect } from 'react'
 import SearchIcon from '@material-ui/icons/Search';
 
-function Search() {
-  
-  
 
-    
+function Search() {
+
+[searchTerm, setSearchTerm] = useState('')
   
 return (
   <>
   <div className="search"></div>
-  <input type='text' defaultValue='' /><SearchIcon></SearchIcon></>
-  
+  <input type='text' defaultValue=''/>
+  <button onClick = {setSearchTerm}>Search</button>
+  </>
   
     
   //   onChange={(newValue) => this.setState({ value: newValue })}
