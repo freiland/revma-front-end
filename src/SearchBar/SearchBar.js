016d7c6c93fed9ca9f8341e7e7fe453313692279
Forrest Reiland <you@example.com>
2021-01-17 16:28:45 -0800

    debug fetch api call

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,24 +1,20 @@
 import SearchBar from 'material-ui-search-bar';
-import React from 'react';
-import { useState, useEffect } from 'react'
-import SearchIcon from '@material-ui/icons/Search';
+import React, { useState, useEffect } from 'react';
 
+function test () {
+  console.log('hello')
+}
 
-function Search() {
-
-const [searchTerm, setSearchTerm] = useState('')
-
-console.log(searchTerm)
-
-const cityInput = setSearchTerm 
-
-useEffect(() => {
-  fetch(`https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${searchTerm}`, {
+function Search () {
+  const [searchTerm, setSearchTerm] = useState('')
+  
+  useEffect(() => {
+    fetch("https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=New%20York", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "7043bd60e6msh017de187a06b311p16fbfejsn6f5cf5064028",
 		"x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com"
-	}, 
+	}
 })
 .then(response => {
 	console.log(response);
@@ -26,21 +22,15 @@ useEffect(() => {
 .catch(err => {
 	console.error(err);
 });
-  
-})
-  
-return (
-  <>
-  <div className="search"></div>
-  <input type='text' value={searchTerm} onChange= {e => setSearchTerm(e.target.value)}/>
-  <button onClick ={e => setSearchTerm(e.target.value)}>Search</button>
-  </>
-  
-    
-  //   onChange={(newValue) => this.setState({ value: newValue })}
-  //   //onRequestSearch={() => doSomethingWith(this.state.value)}
-  // />
-);
-};
+  }, [searchTerm]
 
+  )
+  
+  return (
+    <>
+    <input value={searchTerm}></input>
+    <button onClick={setSearchTerm}>Search</button>
+    </>
+  )
+}
 export default Search;
--- src/Splash/Splash.js
+++ src/Splash/Splash.js
@@ -9,15 +9,6 @@ export default function Splash () {
       
       <div className='text-wrapper'>
       <h1>find your room.<p><Search></Search></p> </h1>
-      
-      
-      <div className="topnav">
-  <a className="active" href="#home">Home</a>
-  <a href="#about">About</a>
-  <a href="#contact">Contact</a>
-  <input type="text" placeholder="Search.."></input>
-    </div>
-
 </div>
       
     </div>
